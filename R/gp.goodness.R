923673e790f884cb52a1b110507fc9f73c53b219
Philipp Benner <benner@molgen.mpg.de>
2015-10-20 10:15:56 +0200

    2015/10/20-10:15:55 (Linux furunkel.molgen.mpg.de unknown)

--- R/gp.goodness.R
+++ R/gp.goodness.R
@@ -34,3 +34,16 @@ r.squared.gp <- function(model, xp = NULL, yp = NULL)
     ss.res <- sum((yp - f)^2)
     1 - ss.res/ss.tot
 }
+
+#' Compute the R^2 of a heteroscedastic Gaussian process
+#'
+#' @param gp Gaussian process
+#' @param xp optional x-values
+#' @param yp optional y-values
+#' @method r.squared gp.heteroscedastic
+#' @export
+
+r.squared.gp.heteroscedastic <- function(model, xp = NULL, yp = NULL)
+{
+    r.squared(gp$gp, xp=xp, yp=yp)
+}
