f97fa5f1679a77a6826809be5147270af747437e
Philipp Benner <benner@molgen.mpg.de>
2015-10-28 10:56:58 +0100

    2015/10/28-10:56:58 (Linux furunkel.molgen.mpg.de unknown)

--- R/gp.goodness.R
+++ R/gp.goodness.R
@@ -15,10 +15,11 @@ r.squared <- function(model, ...)
 #' @param gp Gaussian process
 #' @param xp optional x-values
 #' @param yp optional y-values
+#' @param adjusted compute adjusted R^2
 #' @method r.squared gp
 #' @export
 
-r.squared.gp <- function(model, xp = NULL, yp = NULL)
+r.squared.gp <- function(model, xp = NULL, yp = NULL, adjusted = FALSE)
 {
     gp <- model
 
@@ -32,7 +33,15 @@ r.squared.gp <- function(model, xp = NULL, yp = NULL)
     f <- summarize(gp, xp)$mean
     ss.tot <- sum((yp - m)^2)
     ss.res <- sum((yp - f)^2)
-    1 - ss.res/ss.tot
+    r2 <- 1 - ss.res/ss.tot
+
+    if (adjusted) {
+        n  <- nrow(gp$xp)
+        k  <- dim (gp)
+        stopifnot(n-k-1 > 0)
+        r2 <- 1.0 - (1.0-r2)*(n-1.0)/(n-k-1)
+    }
+    r2
 }
 
 #' Compute the R^2 of a heteroscedastic Gaussian process
@@ -40,10 +49,11 @@ r.squared.gp <- function(model, xp = NULL, yp = NULL)
 #' @param gp Gaussian process
 #' @param xp optional x-values
 #' @param yp optional y-values
+#' @param ... options passed to the r.squared method
 #' @method r.squared gp.heteroscedastic
 #' @export
 
-r.squared.gp.heteroscedastic <- function(model, xp = NULL, yp = NULL)
+r.squared.gp.heteroscedastic <- function(model, xp = NULL, yp = NULL, ...)
 {
-    r.squared(gp$gp, xp=xp, yp=yp)
+    r.squared(gp$gp, xp=xp, yp=yp, ...)
 }
