a30c11b9611a7963cac1bacd3d03723d8fd7090a
Gen Kamita <gk298@cam.ac.uk>
2017-02-03 20:35:55 +0900

    removed all test scripts

--- R/testscripts/test_solvechol.R
+++
@@ -1,7 +0,0 @@
-test_solvechol <- function(W_vector, K, r, n){
-rootW <- sqrt(W_vector)
-B <- diag(n) + rootW %*% t(rootW) * K # (c.f. Rasmussen 2006, Eq. 3.26)
-L <- chol(B)
-#temp <- solve(L * t(L), sweep(r, 2, rootW, "*"))#this sweep is strange, r and rootW should be both vectors.
-temp <- solve(L, solve(t(L), sweep(r, 1, rootW, "*"))
-}
